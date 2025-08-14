d2a6ac8aa916d3325da22b826c4e96c4097ccee2
Mudiwa Nyikavaranda <mudiwanyikavaranda@gmail.com>
2025-08-14 23:35:04 +0200

    Update AppConfig for personal portfolio

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Mudiwa Nyikavaranda Portfolio',
+  title: 'Mudiwa Nyikavaranda Portfolio',
+  description: 'Portfolio showcasing projects and skills of Mudiwa Nyikavaranda',
+  author: 'Mudiwa Nyikavaranda',
   locale_region: 'en-us',
   locale: 'en',
 };
